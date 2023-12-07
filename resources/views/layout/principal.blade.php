0fc073ac048aee6820e37acb32b794cc7b921179
olaormundos <vanderleidasilvaormundo@gmail.com>
2023-12-06 22:23:39 -0300

    Estilizando

--- resources/views/layout/principal.blade.php
+++ resources/views/layout/principal.blade.php
@@ -5,12 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="/css/custom.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <title>Controle de estoque</title>
 </head>
 <body>
+    <nav class="navbar navbar-default">
+        <div class="navbar-header">
+            <a href="/produtos" class="navbar-brand">
+                Estoque
+            </a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href="/produtos">
+                    Listagem
+                </a>
+            </li>
+        </ul>
+    </nav>   
     <div class="container">
         @yield('conteudo')
     </div>
+    <footer class="footer">
+        <p>Sistema de estoque de produtos com Laravel e PHP.</p>
+    </footer>    
 </body>
 </html>
